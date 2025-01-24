824edb2aa9f44607c96a34fa24ca0b932aaa624e
vivek-414 <2024.vivekp@isu.ac.in>
2025-01-24 14:47:19 +0530

    Added Surface area Method

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ class Cylinder:
     def __init__(self, radius, height):
         self.radius = radius
         self.height = height
+        
+    def surface_area(radius,height):
+        return 2*3.14*radius*height+2*3.14*(radius**2)
