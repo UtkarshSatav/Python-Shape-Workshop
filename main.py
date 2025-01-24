b9b9b6f58b04f20a846f41fdc73e5bf2de37ed4b
UtkarshSatav <ianutakrsh@gmail.com>
2025-01-24 14:57:54 +0530

    Added Curved Surface Area Method

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ class Cylinder:
     def __init__(self, radius, height):
         self.radius = radius
         self.height = height
+    
+    def curvedSurfaceArea(self):
+        return 2 * 3.14 * self.radius * self.height
